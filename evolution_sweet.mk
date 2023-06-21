2c4051bd0f201728f708ef6b68c14ec4c0475d15
pure-soul-kk <krishnakripa34567@gmail.com>
2023-06-21 22:09:09 +0530

    sweet: init EvolutionX

--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -12,18 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_ENABLE_BLUR := true
-
-# AlphaDroid Stuff
-ALPHA_MAINTAINER=K R I S H N A
 
 # Device Identifier.
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
