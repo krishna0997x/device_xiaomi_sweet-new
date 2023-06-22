bdf7fd540a1e30e9d5546a2a3ee9754e805d7d87
pure-soul-kk <krishnakripa34567@gmail.com>
2023-06-22 09:40:50 +0530

    sweet: init AlphaDroid

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -11,13 +11,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AlphaDroid stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_MATLOG := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_DISABLE_EPPE := true
+
+# Gapps
+WITH_GAPPS := 2
+
+# Valid for core/full GAPPS (default=true)
+TARGET_USE_GOOGLE_TELEPHONY := false
+
+# AlphaDroid Maintainer
+ALPHA_MAINTAINER := K R I S H N A
 
 # Device Identifier.
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
